8b6788213433a278b7eb546abf25cdec18f554e5
AjayManinder <ajaymaninder1111@gmail.com>
2024-08-13 16:05:40 -0500

    changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import profileImg from '../assets/profile.jpg'; // Replace with your profile image path
+// import profileImg from '../assets/profile.jpg'; // Replace with your profile image path
 
 const Home = () => {
   return (
@@ -7,8 +7,8 @@ const Home = () => {
      {/* <img src={profileImg} alt="Profile" className="profile-img" /> */}
       <h1>AJAY MANINDER</h1>
       <p>Full-Stack React Developer</p>
-      <p>Email: ajaymaninder1111@gmail.com</p>
-      <p>Phone: +1-816-872-8004</p>
+      <p>Email: ajyrdev@gmail.com</p>
+      <p>Phone: +1-913-890-3323</p>
     </section>
   );
 };
